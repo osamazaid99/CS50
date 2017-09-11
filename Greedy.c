25d29112d7ad5c9debcc52c3c3bc8b3194af1a7c
osamazaid99 <31866888+osamazaid99@users.noreply.github.com>
2017-09-11 17:21:47 -0400

    Create Greedy.c

---
+++ Greedy.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+   float change;
+   int count = 0, changeCent;
+    
+    
+    do
+    {
+        printf("O hai! How much change is owed?\n");
+         change = get_float();
+    }
+    while(change<0);
+    
+   change *= 100.0;
+   changeCent = round(change);
+   
+   while(changeCent >= 25)
+   {
+       count++;
+       changeCent-= 25;
+   }
+   
+    while(changeCent >= 10)
+   {
+       count++;
+       changeCent-= 10;
+   }
+   
+    while(changeCent >= 5)
+   {
+       count++;
+       changeCent-= 5;
+   }
+   
+    while(changeCent >= 1)
+   {
+       count++;
+       changeCent-= 1;
+   }
+   
+   printf("%i\n", count);
+}
