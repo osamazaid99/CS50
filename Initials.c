8f27fe3b8e10a4314ffc359d19ac113e372800bc
osamazaid99 <31866888+osamazaid99@users.noreply.github.com>
2017-09-11 17:24:29 -0400

    Create Initials.c

---
+++ Initials.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+    string name = get_string();
+ 
+    printf("%c",toupper(name[0]));
+ 
+ for (int i = 0; i< strlen(name); i++)
+ {
+     if (name[i]== ' ')
+     {
+         printf("%c", toupper(name[i+1]));
+     }
+ }
+ printf("\n");
+}
